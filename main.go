2bd69a318e03423b20e401959e8211167c046e3e
qingfeng <qingfeng.xu.contractor@hilton.com>
2023-07-14 15:23:46 +0800

    Echo plain hostname

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		name, _ := os.Hostname()
-		fmt.Fprintf(w, "reaching: %s", name)
+		fmt.Fprintf(w, "%s", name)
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
